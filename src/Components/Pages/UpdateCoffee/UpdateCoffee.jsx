e5b1fd0b4a7c125f4222d863bd381de6e69791cf
Sazib018 <mahiburislamsazib@gmail.com>
2025-03-01 04:31:37 -0600

    add

--- src/Components/Pages/UpdateCoffee/UpdateCoffee.jsx
+++ src/Components/Pages/UpdateCoffee/UpdateCoffee.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { IoMdArrowBack } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 
 const UpdateCoffee = () => {
@@ -11,60 +12,158 @@ const UpdateCoffee = () => {
         }
         
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-
-        <div className="bg-[#F4F3F0] rounded-lg shadow-lg p-28 max-w-[1320px]">
-            {/* Back to Home */}
-            <Link to="/" className="text-gray-600 hover:text-black flex items-center mb-4">
-                ← <span className="ml-1 text-3xl text-[#374151] font-semibold">Back to home</span>
-            </Link>
-
-            {/* Title */}
-            <h2 className="text-[45px] font-normal text-center text-[#374151] mb-2">Update Existing Coffee Details</h2>
-            <p className="text-center fontRailway px-48 text-[#1B1A1AB2] mb-6">
-            It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
-            </p>
-
-            {/* Form */}
-            <form onSubmit={handleUpdate}  className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div>
-                    <label className="block fontRailway text-gray-700">Name</label>
-                    <input type="text" required placeholder="Enter coffee name" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div>
-                    <label className="block fontRailway text-gray-700">Chef</label>
-                    <input type="text" required placeholder="Enter coffee chef" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div>
-                    <label className="block fontRailway text-gray-700">Supplier</label>
-                    <input type="text" required placeholder="Enter coffee supplier" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div>
-                    <label className="block fontRailway text-gray-700">Taste</label>
-                    <input type="text" required placeholder="Enter coffee taste" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div>
-                    <label className="block fontRailway text-gray-700">Category</label>
-                    <input type="text" required placeholder="Enter coffee category" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div>
-                    <label className="block fontRailway text-gray-700">Details</label>
-                    <input type="text" required placeholder="Enter coffee details" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-                <div className="col-span-1 md:col-span-2">
-                    <label className="block fontRailway text-gray-700">Photo</label>
-                    <input type="text" required placeholder="Enter photo URL" className="input w-full fontRailway border border-[#E3B577] rounded-md" />
-                </div>
-
-                {/* Submit Button */}
-                <div className="col-span-1 md:col-span-2 flex justify-center">
-                    <button  className="bg-[#c49a6c] w-full text-[#331A15] px-6 py-2 rounded-md hover:bg-[#a67c52] transition">
-                    Update Coffee Details
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
+        <div>
+                   <div className="bg-cover 2xl:bg-[url('https://i.ibb.co.com/Ld6pjvmr/11.png')] xl:bg-[url('https://i.ibb.co.com/Ld6pjvmr/11.png')]   lg:bg-none md:bg-none sm:bg-none">
+       
+                       <div className="container mx-auto px-3 md:px-6 py-16 space-y-10">
+                           <Link to="/" className="flex items-center gap-3">
+                               <IoMdArrowBack className="text-2xl" />
+                               <span className="text-4xl font-normal text-[#374151]">
+                                   Back to home
+                               </span>
+                           </Link>
+                           <div className="bg-[#F4F3F0] py-16 px-5 sm:px-10 lg:px-28 rounded space-y-5">
+                               <h3 className=" md:text-5xl text-4xl font-normal text-center">
+                                  Update Coffee Details 
+                               </h3>
+                               <p className="fontRailway font-normal text-[#1B1A1AB2]  md:text-lg text-sm px-1 sm:px-4 lg:px-12 text-center">
+                                   It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
+                               </p>
+                               <form onSubmit={handleUpdate}>
+                                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Name
+                                               </span>
+                                           </div>
+                                           <input type="text" name="name"
+                                               placeholder="Enter coffee name"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Chef
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="chef"
+                                               placeholder="Enter coffee chef"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                   </div>
+                                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Supplier
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="supplier"
+                                               placeholder="Enter coffee supplier"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Taste
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="taste"
+                                               placeholder="Enter coffee taste"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                   </div>
+                                   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Category
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="category"
+                                               placeholder="Enter coffee category"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Price
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="price"
+                                               placeholder="Enter coffee price"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                   </div>
+                                   <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Details
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="details"
+                                               placeholder="Enter coffee details"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                       <label className="form-control">
+                                           <div className="label">
+                                               <span className="label-text fontRailway font-semibold">
+                                                   Photo
+                                               </span>
+                                           </div>
+                                           <input
+                                               type="text"
+                                               name="photoUrl"
+                                               placeholder="Enter photo URL"
+                                               style={{ outline: "none" }}
+                                               className="input fontRailway "
+                                               required
+                                           />
+                                       </label>
+                                   </div>
+                                   <button className="btn btn-block mt-6 border-2 bg-[#D2B48C] text-[#331A15] text-2xl font-normal">
+                                      Update Coffee Details
+                                   </button>
+                               </form>
+                           </div>
+                       </div>
+                   </div>
+               </div>
     );
 };
 
