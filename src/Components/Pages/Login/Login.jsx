175e8af417afc04bbcc01874ccdaf14d2b0a909d
Sazib018 <mahiburislamsazib@gmail.com>
2025-03-02 23:59:42 -0600

    eye icon

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -33,37 +33,37 @@ const Login = () => {
   return (
     <div className="flex items-center mt-8 justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-center text-gray-800">
+        <h2 className="text-2xl fontRailway font-semibold text-center text-gray-800">
           Login to your account
         </h2>
         <hr className="my-4" />
-        
+
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-1">
+            <label className="block fontRailway text-gray-700 font-medium mb-1">
               Email address
             </label>
             <input
               type="email" {...register("email")} 
               placeholder="Enter your email address"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+              className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-1">
+            <label className="block fontRailway text-gray-700 font-medium mb-1">
               Password
             </label>
             <input
               type="password" {...register("password")} 
               placeholder="Enter your password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+              className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-[#c49a6c] text-white py-2 rounded-lg hover:bg-[#7c6247] transition"
+            className="w-full fontRailway bg-[#c49a6c] text-white py-2 rounded-lg hover:bg-[#7c6247] transition"
           >
             Login
           </button>
@@ -71,12 +71,12 @@ const Login = () => {
 
         <button
           onClick={handleGoogleLogin}
-          className="w-full bg-[#9c5e1c] text-white p-2 rounded mt-4"
+          className="w-full fontRailway bg-[#9c5e1c] text-white p-2 rounded mt-4"
         >
           Login with Google
         </button>
 
-        <p className="text-center text-gray-600 mt-4">
+        <p className="text-center fontRailway text-gray-600 mt-4">
           Don’t Have An Account?{" "}
           <Link to="/register" className="text-red-500 font-medium">
             Register
--- src/Components/Pages/Register/Register.jsx
+++ src/Components/Pages/Register/Register.jsx
@@ -1,15 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../Providers/AuthProvider';
+import { useNavigate } from 'react-router-dom';
+import { IoIosEye, IoIosEyeOff } from "react-icons/io";
 
 const Register = () => {
     const { register, handleSubmit } = useForm();
-    const { createUser } = useContext(AuthContext); 
+    const { createUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState(""); 
 
     const onSubmit = (data) => {
         createUser(data.email, data.password) 
             .then(userInfo => {
-                console.log( userInfo.user);
+                console.log("User registered:", userInfo.user);
+                navigate("/login");
             })
             .catch(error => {
                 console.error("Registration Error:", error.message);
@@ -19,54 +26,66 @@ const Register = () => {
     return (
         <div className="flex items-center mt-8 justify-center min-h-screen bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-                <h2 className="text-2xl font-semibold text-center text-gray-800">
+                <h2 className="text-2xl fontRailway font-semibold text-center text-gray-800">
                     Register your account
                 </h2>
                 <hr className="my-4" />
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-1">
+                        <label className="block fontRailway text-gray-700 font-medium mb-1">
                             Name
                         </label>
                         <input
                             type="text" required {...register("name")}
                             placeholder="Enter your name"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-1">
+                        <label className="block fontRailway text-gray-700 font-medium mb-1">
                             Photo URL
                         </label>
                         <input
                             type="text" required {...register("photoURL")} 
                             placeholder="Enter your photo URL"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-1">
+                        <label className="block fontRailway text-gray-700 font-medium mb-1">
                             Email
                         </label>
                         <input
                             type="email" required {...register("email")}
                             placeholder="Enter your email address"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
                         />
                     </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-1">
-                            Password
-                        </label>
-                        <input
-                            type="password" required {...register("password")}
-                            placeholder="Enter your password"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
-                        />
+                    
+                    <div className="mb-4 relative">
+                        <label className="block fontRailway text-gray-700">Password:</label>
+                        <div className="relative">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                className="w-full fontRailway px-4 py-2 border border-gray-300 rounded-lg pr-10"
+                                placeholder="Enter your password"
+                                value={password}
+                                {...register("password", { required: true })}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                type="button"
+                                className="absolute fontRailway inset-y-0 right-3 flex items-center text-gray-500"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <IoIosEye size={25} /> : <IoIosEyeOff size={25} />}
+                            </button>
+                        </div>
                     </div>
+
                     <button
                         type="submit"
-                        className="w-full bg-[#c49a6c] hover:bg-[#5b452d] text-white py-2 rounded-lg"
+                        className="w-full fontRailway bg-[#c49a6c] hover:bg-[#5b452d] text-white py-2 rounded-lg"
                     >
                         Register
                     </button>
